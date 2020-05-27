b885b7e8fd269238f8417ad75374f91a5b6e4089
Marco Nardes <marconardes@gmail.com>
2020-05-27 10:08:32 -0300

    Update
    Update DAO e melhorando os resultados.

--- covd19data/src/org/esfinge/virtuallab/covid/CoronaDao.java
+++ covd19data/src/org/esfinge/virtuallab/covid/CoronaDao.java
@@ -1,34 +1,62 @@
 package org.esfinge.virtuallab.covid;
 
+import java.util.Calendar;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.EqualsExclude;
 import org.esfinge.virtuallab.api.annotations.ServiceDAO;
 import org.esfinge.virtuallab.api.annotations.ServiceMethod;
 import org.esfinge.virtuallab.api.annotations.TableReturn;
 
 import net.sf.esfinge.querybuilder.Repository;
+import net.sf.esfinge.querybuilder.annotation.Greater;
+import net.sf.esfinge.querybuilder.annotation.GreaterOrEquals;
 
 @ServiceDAO(
 		label = "DAO",
 		description = "",
 		url = "jdbc:postgresql://localhost:5432/postgres", 
 		user = "postgres", 
-		password = "postgres", 
+		password = "marco12", 
 		dialect = "org.hibernate.dialect.PostgreSQLDialect")
 public interface CoronaDao extends Repository<CoronaDados>
 	{
 		
 		@ServiceMethod(
-			label = "Listar todas temperaturas",
-			description = "Retorna todos as temperaturas cadastradas no Banco de Dados.")
+			label = "",
+			description = "")
 		@TableReturn
-		public List<CoronaDados> getCoronaDados();
+		public List<CoronaDados> getCoronaDadosOrderByData();
 		
 		@ServiceMethod(
-				label = "Listar todas temperaturas",
-				description = "Retorna todos as temperaturas cadastradas no Banco de Dados.")
+				label = "",
+				description = "")
 		@TableReturn
-		public List<CoronaDados> getCoronaDadosByLocation(String location);
+		public List<CoronaDados> getCoronaDadosByLocationOrderByData(String location);		
 		
+		@ServiceMethod(
+				label = "",
+				description = "")
+		@TableReturn
+		public List<CoronaDados> getCoronaDadosByDataOrderByTotalDeathsDesc(@GreaterOrEquals Calendar data);		
 		
+		@ServiceMethod(
+				label = "",
+				description = "")
+		@TableReturn
+		public List<CoronaDados> getCoronaDadosByDataOrderByNewDeathsDesc(@GreaterOrEquals Calendar data);		
+
+		@ServiceMethod(
+				label = "",
+				description = "")
+		@TableReturn
+		public List<CoronaDados> getCoronaDadosByDataOrderByNewCasesDesc(@GreaterOrEquals Calendar data);		
+
+		@ServiceMethod(
+				label = "",
+				description = "")
+		@TableReturn
+		public List<CoronaDados> getCoronaDadosByDataOrderByTotalCasesDesc(@GreaterOrEquals Calendar data);		
+
+	
 	}
--- covd19data/src/org/esfinge/virtuallab/covid/CoronaProgram.java
+++ covd19data/src/org/esfinge/virtuallab/covid/CoronaProgram.java
@@ -1,11 +1,15 @@
 package org.esfinge.virtuallab.covid;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.esfinge.virtuallab.api.annotations.Combo;
+import org.esfinge.virtuallab.api.annotations.ComboMethod;
 import org.esfinge.virtuallab.api.annotations.Inject;
+import org.esfinge.virtuallab.api.annotations.LineChartReturn;
 import org.esfinge.virtuallab.api.annotations.ServiceClass;
 import org.esfinge.virtuallab.api.annotations.ServiceMethod;
 import org.esfinge.virtuallab.api.annotations.TableReturn;
@@ -30,17 +34,76 @@ public class CoronaProgram
 	@TableReturn
 	public List<CoronaDados> listAllData()
 	{
- 
-		return cd.getCoronaDados();
+		List<CoronaDados> retorno =  this.cd.getCoronaDadosOrderByData();
+
+		return retorno;
 	}
 	
 	@ServiceMethod(
 			label = "Retorna os dados do coronavirus por pais que for selecionado",
 			description = "Seleciona os casos dos países")
 		@TableReturn
-		public List<CoronaDados> listPais(String pais)
+		public List<CoronaDados> listByLocation(@Combo(value = "location") String location)
 		{	
-			return cd.getCoronaDadosByLocation(pais);
+			List<CoronaDados> retorno = this.cd.getCoronaDadosByLocationOrderByData(location);
+			return retorno;
+		}
+	
+	@ComboMethod(value = "location")
+	public Map<String,String> locationCombo()
+	{
+		Map<String, String> paisMap = new HashMap<String, String>();
+		for (CoronaDados iterable_element : listAllData()) {
+			paisMap.put(iterable_element.getLocation(), iterable_element.getLocation());
 		}
 		
+		return paisMap;
+	}
+	
+	@ServiceMethod(
+			label = "Retorna os dados do coronavirus por pais que for selecionado",
+			description = "Seleciona os casos dos países")
+		@TableReturn
+
+	public List<CoronaDados> listByPerNumberOfDeathsToday()
+	{
+		Calendar dataAtual = Calendar.getInstance();
+		dataAtual.add(Calendar.DAY_OF_MONTH, -1);
+		List<CoronaDados> value = cd.getCoronaDadosByDataOrderByNewDeathsDesc(dataAtual);
+		
+		List<CoronaDados> coronatTop10= new ArrayList<CoronaDados>();
+		
+		for (int i = 1; i <= 10; i++) {
+			coronatTop10.add(value.get(i));
+		}
+		
+		return coronatTop10;
+	}
+	
+	@ServiceMethod(
+			label = "Retorna os dados do coronavirus por pais que for selecionado",
+			description = "Seleciona os casos dos países")
+	@LineChartReturn(typeOfChart = "line",
+			dataLabels = "eventDateTime",
+			temporalSeries = true,
+			multipleDataset = true,
+			xAxisShowGridlines = true, 
+			title = "Novos Casos de Corona Virus no pais",
+			yAxisLabel = "Casos",
+			xAxisLabel = "Data",
+			xAxis = {"data"},
+			yAxis= {"newCases"})
+		public List<CoronaDados> graphByLocation(@Combo(value = "location") String location)
+		{	
+			List<CoronaDados> retorno = this.cd.getCoronaDadosByLocationOrderByData(location);
+			List<CoronaDados> retorno2 = new ArrayList<CoronaDados>();
+			for (int i = retorno.size()-50; i < retorno.size(); i++) {
+				retorno2.add(retorno.get(i));
+			}
+			
+			return retorno2;
+		}
+
+
+	
 }
